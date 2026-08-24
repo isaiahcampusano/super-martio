3f063b57888161d8458fcbff4cbf0e2c5c5c34de
404zay <inc67651@uga.edu>
2026-08-24 18:12:54 -0400

    Fix walkers falling off ledges

--- scenes/enemies/walker.gd
+++ scenes/enemies/walker.gd
@@ -3,7 +3,7 @@ extends PocketEnemy
 
 func _init() -> void:
 	speed = 72.0
-	turns_at_edges = false
+	turns_at_edges = true
 	body_color = Color("bd77ff")
 	body_size = Vector2(36.0, 32.0)
 	maximum_health = 1
